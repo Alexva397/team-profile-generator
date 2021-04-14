7f519664b04a1506cb41deeb2e013b3e55bc8078
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-04-14 00:35:17 -0500

    started wrting initial prompts for manager, messing around with creating a continue button before any prompts that would close the program

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
 // include packages
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
-
+const managerPrompt = () => {
+    return inquirer.prompt([
+        {
+            name: 'intro',
+            type: 'confirm',
+            message: 'Please answer the following questions about the team manager.\nPress y to begin.',
+            validate: function(response) {
+                if(response !== 'y') {
+                    return;
+                }
+                return true;
+            }
+        },
+        {
+            name: 'name',
+            type: 'input',
+            message: 'What is their name?',
+        },
+        {
+            name: 'id',
+            type: 'input',
+            message: 'What is their ID number?',
+        },
+        {
+            name: 'email',
+            type: 'input',
+            message: 'What is their email address?',
+        },
+        {
+            name: 'office',
+            type: 'input',
+            message: 'What is their office number?',
+        },
+    ])
+};
 
 
 
@@ -20,7 +57,9 @@ const fs = require('fs');
 
 
 const init = () => {
-
+    console.log('Welcome to the Team Profile Generator!\n----------------------------------------');
+    managerPrompt()
+    .then(console.log(data));
 };
 
 
