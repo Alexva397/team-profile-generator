ac75031892223f3e8d8548739706c7ed2666d5b7
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-04-15 17:58:41 -0500

    added parseint to employee.js and manager.js to have office# and id# return as an integer, wrote function to create new manager and prompt for new team member

--- index.js
+++ index.js
@@ -5,19 +5,21 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
-const managerPrompt = () => {
+const teamArray = [];
+
+const managerInfo = () => {
     return inquirer.prompt([
-        {
-            name: 'intro',
-            type: 'confirm',
-            message: 'Please answer the following questions about the team manager.\nPress y to begin.',
-            validate: function(response) {
-                if(response !== 'y') {
-                    return;
-                }
-                return true;
-            }
-        },
+        // {
+        //     name: 'intro',
+        //     type: 'confirm',
+        //     message: 'Please answer the following questions about the team manager.\nPress y to begin.',
+        //     validate: function(response) {
+        //         if(response !== 'y') {
+        //             return;
+        //         }
+        //         return true;
+        //     }
+        // },
         {
             name: 'name',
             type: 'input',
@@ -41,15 +43,30 @@ const managerPrompt = () => {
     ])
 };
 
+const createManager = (data) => {
+    const managerName = data.name;
+    const managerID = data.id;
+    const managerEmail = data.email;
+    const offficeNum = data.office;
+    const newManager = new Manager(managerName, managerID, managerEmail, offficeNum);
+    teamArray.push(newManager);
+    addTeamMember();
+};
 
 
-
-
-
-
-
-
-
+const addTeamMember = () => {
+    inquirer.prompt([
+        {
+            name: 'newMember',
+            type: 'list',
+            message: 'Would you like to add another team member?',
+            choices: ['Engineer', 'Intern', 'There are no more members.'],
+        },
+    ])
+    .then((data) => {
+        console.log(data.newMember);
+    })
+}
 
 
 
@@ -58,8 +75,10 @@ const managerPrompt = () => {
 
 const init = () => {
     console.log('Welcome to the Team Profile Generator!\n----------------------------------------');
-    managerPrompt()
-    .then(console.log(data));
+    managerInfo()
+    // .then((data) => console.log(data))
+    .then((data) => createManager(data))
+    .catch((err) => console.error(err));
 };
 
 
